150015e13278381b364bd89b1d880232e10b6282
Dinçer Kaan Turanlı <80959458+dincerkaan@users.noreply.github.com>
2024-12-25 10:15:42 +0300

    Time: 0 ms (100%), Space: 4.5 MB (98.84%) - LeetHub

---
+++ 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.go
@@ -0,0 +1,13 @@
+func checkIfExist(arr []int) bool {
+    for i := 0; i < len(arr); i++{
+        for j := 0; j < len(arr); j++{
+            if(i == j){
+                continue
+            }
+            if arr[i] == arr[j] * 2{
+                return true 
+            }
+        }
+    }
+    return false
+}
